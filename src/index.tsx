b27b7d0af1fb98ae353715777a3b0c8de087eb27
indrephone <indrephone@gmail.com>
2024-09-05 14:08:00 +0300

    wrap index.tsx with UsersProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { UsersProvider } from './contexts/UsersContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLDivElement
 );
 root.render(
     <BrowserRouter>
-    
+      <UsersProvider>
            <App />
-    
+      </UsersProvider>
     </BrowserRouter>
 );
